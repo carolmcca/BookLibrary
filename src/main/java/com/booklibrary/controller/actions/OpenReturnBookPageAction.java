b00bf7d841a24f79a6a1ebea128630d7ada61cd6
carolmcca <77802421+carolmcca@users.noreply.github.com>
2022-09-06 17:08:44 +0100

    Update src/main/java/com/booklibrary/controller/actions/OpenReturnBookPageAction.java
    Co-authored-by: Henrique Nunes <hrn2001@gmail.com>

--- src/main/java/com/booklibrary/controller/actions/OpenReturnBookPageAction.java
+++ src/main/java/com/booklibrary/controller/actions/OpenReturnBookPageAction.java
@@ -5,8 +5,8 @@ import com.booklibrary.model.Database;
 import com.booklibrary.view.pages.ReturnBookPage;
 
 public class OpenReturnBookPageAction extends ButtonAction {
-    public OpenReturnBookPageAction(Book book, Database database){
+    public OpenReturnBookPageAction(Database database, Book book){
         this.buttonText = "Devolver";
-        this.page = new ReturnBookPage(book, database);
+        this.page = new ReturnBookPage(database, book);
     }
 }
